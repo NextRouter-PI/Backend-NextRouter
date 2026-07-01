737f39a43cb35fc030ef05aaed0e2dee10835f04
Pauloartur-23 <pauloartur.ifc@gmail.com>
2026-07-01 14:39:27 -0300

    fix: Ajustando https vercel

--- app/settings.py
+++ app/settings.py
@@ -22,12 +22,14 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
     'http://localhost:5173',
+    'https://frontend-next-router-ruby.vercel.app/'
 ]
 CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://frontend-next-router-ruby.vercel.app/'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
