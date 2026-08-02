094f45347879b9f9f5d8fcfb07f389947a9c21fd
IanMV <ianmatheusv54@gmail.com>
2026-08-02 16:12:37 -0300

    build: remoção dependências não utilizadas e adição de necessárias (o graph_models não funcionava sem ter que instalar o vgraphiz no linux, então foi adicionado pytdot junto do pillow); criação de script que prepara o ambiente de desenvolvimento: migrações no banco e criando .env; alterado sessão de scripts pdm para adição de novo script setup_env.py junto do script set_my_ip.py; adição de dois nomes aos autores do projeto

---
+++ scripts/setup_env.py
@@ -0,0 +1,64 @@
+import os
+import subprocess
+
+
+def setup_project():
+    print('Instalando dependências...')
+    subprocess.run(['pdm', 'install'], check=True, shell=False)
+
+    env_file = '.env'
+    if not os.path.exists(env_file):
+        with open(env_file, 'w', encoding='utf-8') as f:
+            f.write('MODE=DEVELOPMENT\n')
+            f.write('DEBUG=True\n')
+            f.write('SECRET_KEY=exemplo\n')
+            f.write('CLOUDINARY_URL=cloudinary://exemplo.com/media/\n')
+            f.write('EMAIL_HOST_USER=seu@email.com\n')
+            f.write('EMAIL_HOST_PASSWORD=\n')
+            f.write('EMAIL_HOST=\n')
+        print()
+        print(f'Arquivo {env_file} criado com variáveis padrão.')
+    else:
+        print()
+        print(f'O arquivo {env_file} já existe. Pulando criação.')
+
+    print()
+    print('Preparando banco de dados...')
+    subprocess.run(
+        ['pdm', 'run', 'python', 'manage.py', 'makemigrations', 'uploader', 'router', 'authenticator'],
+        check=True,
+        shell=False,
+    )
+
+    subprocess.run(['pdm', 'run', 'python', 'manage.py', 'migrate'], check=True, shell=False)
+
+    print()
+    print('Gerando MER...')
+    subprocess.run(
+        [
+            'pdm',
+            'run',
+            'python',
+            'manage.py',
+            'graph_models',
+            '--pydot',
+            '--arrow-shape',
+            'normal',
+            '--color-code-deletions',
+            '--hide-edge-labels',
+            '--disable-sort-fields',
+            '-S',
+            '-g',
+            '-o',
+            'core.png',
+            'authenticator',
+            'uploader',
+            'router',
+        ],
+        check=True,
+        shell=False,
+    )
+
+
+if __name__ == '__main__':
+    setup_project()
