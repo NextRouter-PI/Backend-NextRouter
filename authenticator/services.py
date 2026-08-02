a2d8890967c5a7ab813a48444d4a70b823742afe
IanMV <ianmatheusv54@gmail.com>
2026-08-02 16:35:17 -0300

    arquivos com tarefas pendentes...

--- authenticator/services.py
+++ authenticator/services.py
@@ -1,6 +1,8 @@
 from django.core.mail import EmailMessage
 
+# TODO - Criar template HTML estilizado para o email.
+
 
 def send_email_token(email, code, subject):
     email = EmailMessage(subject=subject, body=code, to=[email])
-    return email.send()
+    return email.send()  # TODO - Transformar em try/except
