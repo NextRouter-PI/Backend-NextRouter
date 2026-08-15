58a5faa00cf8c73cecf642df1458cc269f840f73
IanMV <ianmatheusv54@gmail.com>
2026-08-15 19:16:20 -0300

    refactor: refatora lógica de serializer do CompanyRouteGroup

--- authenticator/serializers/company_route_group.py
+++ authenticator/serializers/company_route_group.py
@@ -1,9 +1,33 @@
 from rest_framework.serializers import ModelSerializer
 
+from authenticator.models.company import Company
 from authenticator.models.company_route_group import CompanyRouteGroup
 
 
-class CompanyRouteGroupSerializer(ModelSerializer):
+class CompanyRouteGroupListAndRetrieveSerializer(ModelSerializer):
+    class Meta:
+        model = CompanyRouteGroup
+        fields = (
+            'name',
+            'common_cep',
+        )
+
+
+class CompanyRouteGroupCreateSerializer(ModelSerializer):
+    # Cria o grupo de rotas com relacionado ao usuário que fez a requisição.
+    def perform_create(self, serializer):
+        company = Company.objects.get(user=self.request.user)
+        serializer.save(company=company)
+
+    class Meta:
+        model = CompanyRouteGroup
+        fields = (
+            'name',
+            'common_cep',
+        )
+
+
+class CompanyRouteGroupPatchSerializer(ModelSerializer):
     class Meta:
         model = CompanyRouteGroup
         fields = (
